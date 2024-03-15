1891ade1b8b2382bac555b3349859a0fa9c2f7ca
surbhitt <surbhit.thakur@gmail.com>
2024-03-15 17:08:05 +0530

    final

--- main.go
+++ main.go
@@ -22,22 +22,48 @@ func newTemplate() *Templates {
     }
 }
 
-type Count struct {
-    Count int
+type Contact struct {
+    Name string
+    Email string
+}
+
+func newContact(name, email string) Contact {
+    return Contact {
+        Name: name,
+        Email: email,
+    }
+}
+
+type Contacts = []Contact
+
+type Data struct {
+    Contacts Contacts
+}
+
+func newData() Data {
+    return Data{
+        Contacts: []Contact{
+            newContact("surbhit", "surbhit@gmail.com"),
+            newContact("tushar", "tushar@gmail.com"),
+        },
+    }
 }
 
 func main(){
     e := echo.New()
     e.Use(middleware.Logger())
             
-    count := Count {Count: 0}
+    data := newData()
+    // for _, x := range data.contacts {
+    //     fmt.Printf("name: %s, email: %s\n", x.name, x.email)
+    // }
     e.Renderer = newTemplate()
     e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index", count)
+        return c.Render(200, "index", data)
     })
-    e.POST("/count", func(c echo.Context) error {
-        count.Count++
-        return c.Render(200, "count", count)
+    e.POST("/contacts", func(c echo.Context) error {
+        data.Contacts = append(data.Contacts, newContact(c.FormValue("name"), c.FormValue("email")))
+        return c.Render(200, "display", data)
     })
     e.Logger.Fatal(e.Start(":8080"))
 }
